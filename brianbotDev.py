1f0a9d5d9f0335c948c0acd6e40f09b6f8ca1cc2
hat-7777777 <thebuilder190@gmail.com>
2019-12-16 11:53:46 -0800

    Add brianbotDev.py

--- brianbotDev.py
+++ brianbotDev.py
@@ -3,6 +3,9 @@ from discord.ext import commands
 from pydub import AudioSegment
 from pydub.playback import play
 
+#Replace "INSERT TOKEN HERE" with your Discord bot token
+token = "NjUxMTQ4NjcyMTY3NTc1NTU0.XfPtXQ.edJvXcq7W6BvzezRIOXLqR3BVsY"
+
 global playing
 playing = False
 client = commands.Bot(command_prefix="!brian")
@@ -47,5 +50,5 @@ async def tts(ctx, *, payload):
                 playing = False
                 await ctx.send("Done :thumbsup:")
 
-#Replace "INSERT TOKEN HERE" with your Discord bot token
-client.run("INSERT TOKEN HERE")
+
+client.run(token)
