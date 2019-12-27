407b2672e69269a3359c6fd67d34dbc4c84f1d49
hat-7777777 <57338967+hat-7777777@users.noreply.github.com>
2019-12-26 21:57:34 -0800

    Added mute and help commands, fixes
    New mute and unmute command (!brian mute, !brian unmute) to disable TTS for owner use only.  New help command (!brian help) for basic overview of commands, had to overwrite default help command included with discord.py. Changed command prefix to have a space after "!brian " allowing commands to have a space between prefix and command, increasing user-friendliness. Changed game message to "!brian help". Added and changed some comments.

--- brianbotDevHome.py
+++ brianbotDevHome.py
@@ -3,51 +3,98 @@ from discord.ext import commands
 from pydub import AudioSegment
 from pydub.playback import play
 
-#Replace "INSERT TOKEN HERE" with your Discord bot token
-token = "NjUxMTQ4NjcyMTY3NTc1NTU0.XgV2AA.FckpM0P8piybx4xujysx0P7mQPQ"
+# Replace "INSERT TOKEN HERE" with your Discord bot token
+token = "NjUxMTQ4NjcyMTY3NTc1NTU0.XgV5OQ.z7QvKRySL0uGmvuMcCJjzJsH8rQ"
 
 global playing
+global muted
 playing = False
-client = commands.Bot(command_prefix="!brian")
+muted = False
+client = commands.Bot(command_prefix="!brian ")
+
+client.remove_command("help")
 
 @client.event
 async def on_ready():
-        print("Brian is now active")
-        await client.change_presence(activity=discord.Game("!briantts <message>"))
+    print(f"Brian logged in as {client.user.name} with id {client.user.id}")
+    await client.change_presence(activity=discord.Game("!brian help"))
 
-async def playsound(audiofile):
-        await play(audiofile)
+
+# function that didnt work for asynchronous tasks, maybe fix it later
+# async def playsound(audiofile):
+#        await play(audiofile)
+
+###COMMANDS###
+
+# help command
+@client.command()
+async def help(ctx):
+       await ctx.send("```HELP \n!brian help: Shows this message\n!brian tts <message>: Play a "
+                      "message with Brian Text-To-Speech™\n!brian mute: Disable text-to-speech (Owner only)\n"
+                      "!brian unmute: Enable text-to-speech (Owner only)```")
+
+# muting commands
+@client.command()
+@commands.is_owner()
+async def mute(ctx):
+    global muted
+    if muted == True:
+        await ctx.send("Already muted. Use **!brian unmute** to unmute.")
+    else:
+        muted = True
+        await ctx.send("Muted audio :mute:")
+
 
 @client.command()
-async def tts(ctx, *, payload):
-        global playing
-        if playing == True:
-                ctx.send("Message is already being played, hold on!")
+@commands.is_owner()
+async def unmute(ctx):
+    global muted
+    if muted == True:
+        muted = False
+        extra = random.randint(0, 29)
+        if extra == 0:
+            await ctx.send("kill me")
         else:
-                await ctx.send(f'Playing message: "{payload}" :arrow_forward:')
-                playing = True
-                print(f"incoming message: {payload}")
-                req = requests.get("https://api.streamelements.com/kappa/v2/speech?voice=Brian&text=" + str(payload))
-                print("getting audio from", req.url)
+            await ctx.send("Unmuted audio :loud_sound:")
+    else:
+        await ctx.send("Already unmuted. Use **!brian mute** to mute.")
 
-                print("creating file")
-                r1 = random.randint(1,1000000)
-                randfile = str(r1)+".mp3"
 
-                print(f"writing audio to {randfile}")
-                with open(randfile, "wb") as my_file:
-                        my_file.write(req.content)
+# text to speech command
+@client.command()
+async def tts(ctx, *, payload):
+    global playing
+    global muted
+    if muted == True:
+        await ctx.send("Muted, unable to play message until unmuted.")
+    elif playing == True:
+        await ctx.send("Message is already being played, hold on!")
+    else:
+        await ctx.send(f'Playing message: "{payload}" :arrow_forward:')
+        playing = True
+        print(f"incoming message: {payload}")
+        req = requests.get("https://api.streamelements.com/kappa/v2/speech?voice=Brian&text=" + str(payload))
+        print("getting audio from", req.url)
 
-                print("preparing audio file")
-                audiofile = AudioSegment.from_mp3(randfile)
+        print("creating file")
+        r1 = random.randint(1, 1000000)
+        randfile = str(r1) + ".mp3"
 
-                print("playing audio")
-                play(audiofile)
-                #playsound(audiofile)
+        print(f"writing audio to {randfile}")
+        with open(randfile, "wb") as my_file:
+            my_file.write(req.content)
 
-                print(f"removing file {randfile}")
-                os.remove(randfile)
-                playing = False
-                await ctx.send("Done :thumbsup:")
+        print("preparing audio file")
+        audiofile = AudioSegment.from_mp3(randfile)
+
+        print("playing audio")
+        play(audiofile)
+        # playsound(audiofile)
+
+        print(f"removing file {randfile}")
+        os.remove(randfile)
+        playing = False
+        await ctx.send("Done :thumbsup:")
+
 
 client.run(token)
